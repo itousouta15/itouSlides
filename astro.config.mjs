c1cfae29bffa17ed65b085106a478dd8c9b69cc3
itousouta15 <hi@itousouta.me>
2026-06-21 16:32:19 +0800

    Feat: add theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://slides.elvismao.com",
+	site: "https://slides.itousouta15.tw/",
 	base: "/",
 	output: "static"
 });
