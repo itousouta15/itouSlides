5fa448187f20d9fc994fe8cc94abfe50914cbdfc
itousouta15 <hi@itousouta.me>
2026-07-14 14:54:42 +0800

    fix: update URLs to reflect domain change from itousouta15.tw to itousouta.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://slides.itousouta15.tw/",
+	site: "https://slides.itousouta.me/",
 	base: "/",
 	output: "static"
 });
